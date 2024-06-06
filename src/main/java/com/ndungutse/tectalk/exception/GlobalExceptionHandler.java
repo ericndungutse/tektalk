449b2e99f3c7e5f326f3ff03b4cd589f4f997197
ericndungutse <dav.ndungutse@gmail.com>
2024-06-06 21:02:04 +0200

    feat: Customize validation errors

--- src/main/java/com/ndungutse/tectalk/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ndungutse/tectalk/exception/GlobalExceptionHandler.java
@@ -1,18 +1,27 @@
 package com.ndungutse.tectalk.exception;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.ndungutse.tectalk.dto.ErrorDetails;
 
 // One that send errors to the client
+// extends ResponseEntityExceptionHandler: Helps to add functionality of customizing validation errors
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         // Handle Specific Exception
         // THis @ used to handle specific errors
         @ExceptionHandler(ResourceNotFoundException.class)
@@ -45,7 +54,30 @@ public class GlobalExceptionHandler {
                                 webRequest.getDescription(false));
 
                 // Log the excetion for debugging and send 500 server error
-
                 return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        // Validation Errors by RespinEntityExceptionHandler
+
+        @Override
+        @Nullable
+        protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                        HttpHeaders headers,
+                        HttpStatusCode status,
+                        WebRequest request) {
+
+                Map<String, String> errors = new HashMap<>();
+
+                ex.getBindingResult().getAllErrors().forEach((error) -> {
+                        System.out.println("🔥🔥🔥🔥 EROR ******************" + error.getObjectName()
+                                        + error.getDefaultMessage());
+                        String fieldName = ((FieldError) error).getField();
+                        String message = error.getDefaultMessage();
+
+                        errors.put(fieldName, message);
+
+                });
+
+                return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
         }
 }
