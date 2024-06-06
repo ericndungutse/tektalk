1f964de641231b25934d5cf92a6ba8f18f0f446d
ericndungutse <dav.ndungutse@gmail.com>
2024-06-06 17:43:39 +0200

    feat: Setup global error handling and handle resource not found exception

---
+++ src/main/java/com/ndungutse/tectalk/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.ndungutse.tectalk.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import com.ndungutse.tectalk.dto.ErrorDetails;
+
+// One that send errors to the client
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    // Handle Specific Exception
+    // THis @ used to handle specific errors
+    @ExceptionHandler(ResourceNotFoundException.class)
+    private ResponseEntity<ErrorDetails> handleResourceNotFound(
+            ResourceNotFoundException exception,
+            WebRequest webRequest) {
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+
+    }
+
+    // Handle global exceptions
+}
